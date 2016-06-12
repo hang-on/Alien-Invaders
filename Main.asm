0a62841f171895f65fec662fc82a7edc0be8c3f9
hang-on <asje@me.com>
2016-06-12 14:46:37 +0200

    Load background image

--- Main.asm
+++ Main.asm
@@ -1,7 +1,6 @@
 .include "Base.inc"
 
-.bank 0
-
+.bank 0 slot 0
 .org $0038
 ; ---------------------------------------------------------------------------
 .section "!VDP interrupt" force
@@ -18,12 +17,27 @@
 .ramsection "Main variables" slot 3
 
 .ends
+.bank 0 slot 0
 ; -----------------------------------------------------------------------------
 .section "Main" free
 ; -----------------------------------------------------------------------------
   SetupMain:
 
-  
+    ld a,0
+    ld b,PaletteEnd-Palette
+    ld hl,Palette
+    call LoadCRam
+
+    ld bc,TilesEnd-Tiles
+    ld de,0
+    ld hl,Tiles
+    call LoadVRam
+
+    ld bc,TilemapEnd-Tilemap
+    ld de,NAME_TABLE_START
+    ld hl,Tilemap
+    call LoadVRam
+
 
     ld a,ENABLE_DISPLAY_ENABLE_FRAME_INTERRUPTS_NORMAL_SPRITES
     ld b,1
@@ -37,3 +51,24 @@
   jp Main
 
 .ends
+
+
+.bank 1 slot 1
+  .include "Invaderlib.inc"
+
+
+
+.bank 2 slot 2
+; -----------------------------------------------------------------------------
+  .section "Data" free
+; -----------------------------------------------------------------------------
+    Tiles:
+      .include "Screen-1-Tiles.inc"
+    TilesEnd:
+    Tilemap:
+      .include "Screen-1-Tilemap.inc"
+    TilemapEnd:
+    Palette:
+      .include "Screen-1-Palette.inc"
+    PaletteEnd:
+  .ends
