289fe2ecc3f7f975f4b48749095ee244f8d4d2b1
hang-on <asje@me.com>
2016-07-02 22:49:54 +0200

    goodbye

--- Main.asm
+++ Main.asm
@@ -62,7 +62,7 @@
     ; Initialize vdp (assume blanked screen and interrupts off):
     LoadImage MockupAssets,MockupAssetsEnd
 
-    ld a,FULL_SCROLL_SHOW_LEFT_COLUMN_KEEP_SPRITES_ENABLE_RASTER_INT
+    ld a,FULL_SCROLL_BLANK_LEFT_COLUMN_KEEP_SPRITES_ENABLE_RASTER_INT
     ld b,0
     call SetRegister
 
@@ -115,14 +115,96 @@
   .endm
 
   BattleRasterEffectTable1:
-    MakeRasterEffectTable -4, ALIGN_SLICES
+    MakeRasterEffectTable -12, ALIGN_SLICES
   BattleRasterEffectTable2:
+    MakeRasterEffectTable -12, SKEW_SLICES
+  BattleRasterEffectTable3:
+    MakeRasterEffectTable -10, ALIGN_SLICES
+  BattleRasterEffectTable4:
+    MakeRasterEffectTable -10, SKEW_SLICES
+  BattleRasterEffectTable5:
+    MakeRasterEffectTable -8, ALIGN_SLICES
+  BattleRasterEffectTable6:
+    MakeRasterEffectTable -8, SKEW_SLICES
+  BattleRasterEffectTable7:
+    MakeRasterEffectTable -6, ALIGN_SLICES
+  BattleRasterEffectTable8:
+    MakeRasterEffectTable -6, SKEW_SLICES
+  BattleRasterEffectTable9:
+    MakeRasterEffectTable -4, ALIGN_SLICES
+  BattleRasterEffectTable10:
     MakeRasterEffectTable -4, SKEW_SLICES
+  BattleRasterEffectTable11:
+    MakeRasterEffectTable -2, ALIGN_SLICES
+  BattleRasterEffectTable12:
+    MakeRasterEffectTable -2, SKEW_SLICES
+  BattleRasterEffectTable13:
+    MakeRasterEffectTable 0, ALIGN_SLICES
+  BattleRasterEffectTable14:
+    MakeRasterEffectTable 0, SKEW_SLICES
+  BattleRasterEffectTable15:
+    MakeRasterEffectTable 2, ALIGN_SLICES
+  BattleRasterEffectTable16:
+    MakeRasterEffectTable 2, SKEW_SLICES
+
+  BattleRasterEffectTable17:
+    MakeRasterEffectTable 4, ALIGN_SLICES
+  BattleRasterEffectTable18:
+    MakeRasterEffectTable 4, SKEW_SLICES
+  BattleRasterEffectTable19:
+    MakeRasterEffectTable 6, ALIGN_SLICES
+  BattleRasterEffectTable20:
+    MakeRasterEffectTable 6, SKEW_SLICES
+  BattleRasterEffectTable21:
+    MakeRasterEffectTable 8, ALIGN_SLICES
+  BattleRasterEffectTable22:
+    MakeRasterEffectTable 8, SKEW_SLICES
+  BattleRasterEffectTable23:
+    MakeRasterEffectTable 10, ALIGN_SLICES
+  BattleRasterEffectTable24:
+    MakeRasterEffectTable 10, SKEW_SLICES
+  BattleRasterEffectTable25:
+    MakeRasterEffectTable 12, ALIGN_SLICES
+  BattleRasterEffectTable26:
+    MakeRasterEffectTable 12, SKEW_SLICES
+
+
+  BattleRasterEffectTable27:
+    MakeRasterEffectTable 10, ALIGN_SLICES
+  BattleRasterEffectTable28:
+    MakeRasterEffectTable 10, SKEW_SLICES
+  BattleRasterEffectTable29:
+    MakeRasterEffectTable 12, ALIGN_SLICES
+  BattleRasterEffectTable30:
+    MakeRasterEffectTable 12, SKEW_SLICES
+  BattleRasterEffectTable31:
+    MakeRasterEffectTable 14, ALIGN_SLICES
+  BattleRasterEffectTable32:
+    MakeRasterEffectTable 14, SKEW_SLICES
+
 
   BattleRasterEffectMetaTable:
     ; A table of pointers to Battle Raster Effects which create the illusion
     ; of a moving alien army.
     .dw BattleRasterEffectTable1, BattleRasterEffectTable2,
+    .dw BattleRasterEffectTable3, BattleRasterEffectTable4,
+    .dw BattleRasterEffectTable5, BattleRasterEffectTable6,
+    .dw BattleRasterEffectTable7, BattleRasterEffectTable8,
+    .dw BattleRasterEffectTable9, BattleRasterEffectTable10,
+    .dw BattleRasterEffectTable11, BattleRasterEffectTable12,
+    .dw BattleRasterEffectTable13, BattleRasterEffectTable14,
+    .dw BattleRasterEffectTable15, BattleRasterEffectTable16,
+
+    .dw BattleRasterEffectTable17, BattleRasterEffectTable18,
+    .dw BattleRasterEffectTable19, BattleRasterEffectTable20,
+    .dw BattleRasterEffectTable21, BattleRasterEffectTable22,
+    .dw BattleRasterEffectTable23, BattleRasterEffectTable24,
+
+    ;.dw BattleRasterEffectTable25, BattleRasterEffectTable26,
+    ;.dw BattleRasterEffectTable27, BattleRasterEffectTable28,
+    ;.dw BattleRasterEffectTable29, BattleRasterEffectTable30,
+    ;.dw BattleRasterEffectTable31, BattleRasterEffectTable32,
+
   BattleRasterEffectMetaTableEnd:
 
 .ends
