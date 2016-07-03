92c1dfe51edc95dae0e1ad8d0a5bbbb14a4bf441
hang-on <asje@me.com>
2016-07-03 23:23:14 +0200

    Major improvement of raster table pointer handling

--- Main.asm
+++ Main.asm
@@ -44,17 +44,13 @@
 .ramsection "Battle loop variables" slot 3
   CurrentRasterEffectPtr dw
   RasterEffectMetaTableIndex db
-  MyPointer dw
 .ends
 ; -----------------------------------------------------------------------------
 .section "Battle Loop" free
 ; -----------------------------------------------------------------------------
   SetupBattleLoop:
     ; Initialize variables:
-    ;GetNextWord RasterEffectMetaTableIndex, BattleRasterEffectMetaTable, BattleRasterEffectMetaTableEnd
-
-    ld hl,BattleRasterEffectTable1
-    ld (MyPointer),hl
+    ld hl,BattleRasterEffectTablesStart
     ld (CurrentRasterEffectPtr),hl
 
     ld a,RASTER_INTERRUPT_VALUE
@@ -88,21 +84,19 @@
     call Timer.Countdown
     call Timer.IsDone
     jp nc,SkipEnemyMovement
-      ;debug:
       ld a,ENEMY_MOVE_INTERVAL
       call Timer.Setup
-      ;GetNextWord RasterEffectMetaTableIndex, BattleRasterEffectMetaTable, BattleRasterEffectMetaTableEnd
-      ld hl,CurrentRasterEffectPtr
-      .rept 6
-        inc (hl)
-      .endr
-      ;ld (CurrentRasterEffectPtr),hl
-      ld hl, MyPointer
-      inc (hl)
-      inc (hl)
-      WordMatch MyPointer, BattleRasterEffectTable6
+      ld hl,CurrentRasterEffectPtr  ; FIXME: Comment this up!
+      ld a,(hl)
+      inc hl
+      ld h,(hl)
+      ld l,a
+      ld de,6
+      add hl,de
+      ld (CurrentRasterEffectPtr),hl
+      WordMatch CurrentRasterEffectPtr, BattleRasterEffectTablesEnd
       jp nc,+
-        ld hl,BattleRasterEffectTable1
+        ld hl,BattleRasterEffectTablesStart
         ld (CurrentRasterEffectPtr),hl
       +:
 
@@ -110,9 +104,6 @@
 
   jp BattleLoop
 
-  MyLabel:
-    nop
-    jp MyLabel
 .ends
 
 .bank 1 slot 1
@@ -136,99 +127,48 @@
     .endif
   .endm
 
-  BattleRasterEffectTable1:
-    MakeRasterEffectTable -12, ALIGN_SLICES
-  BattleRasterEffectTable2:
-    MakeRasterEffectTable -12, SKEW_SLICES
-  BattleRasterEffectTable3:
-    MakeRasterEffectTable -10, ALIGN_SLICES
-  BattleRasterEffectTable4:
-    MakeRasterEffectTable -10, SKEW_SLICES
-  BattleRasterEffectTable5:
-    MakeRasterEffectTable -8, ALIGN_SLICES
-  BattleRasterEffectTable6:
-    MakeRasterEffectTable -8, SKEW_SLICES
-  BattleRasterEffectTable7:
-    MakeRasterEffectTable -6, ALIGN_SLICES
-  BattleRasterEffectTable8:
-    MakeRasterEffectTable -6, SKEW_SLICES
-  BattleRasterEffectTable9:
-    MakeRasterEffectTable -4, ALIGN_SLICES
-  BattleRasterEffectTable10:
-    MakeRasterEffectTable -4, SKEW_SLICES
-  BattleRasterEffectTable11:
-    MakeRasterEffectTable -2, ALIGN_SLICES
-  BattleRasterEffectTable12:
-    MakeRasterEffectTable -2, SKEW_SLICES
-  BattleRasterEffectTable13:
+  BattleRasterEffectTablesStart:
     MakeRasterEffectTable 0, ALIGN_SLICES
-  BattleRasterEffectTable14:
     MakeRasterEffectTable 0, SKEW_SLICES
-  BattleRasterEffectTable15:
     MakeRasterEffectTable 2, ALIGN_SLICES
-  BattleRasterEffectTable16:
     MakeRasterEffectTable 2, SKEW_SLICES
-
-  BattleRasterEffectTable17:
     MakeRasterEffectTable 4, ALIGN_SLICES
-  BattleRasterEffectTable18:
     MakeRasterEffectTable 4, SKEW_SLICES
-  BattleRasterEffectTable19:
     MakeRasterEffectTable 6, ALIGN_SLICES
-  BattleRasterEffectTable20:
     MakeRasterEffectTable 6, SKEW_SLICES
-  BattleRasterEffectTable21:
     MakeRasterEffectTable 8, ALIGN_SLICES
-  BattleRasterEffectTable22:
     MakeRasterEffectTable 8, SKEW_SLICES
-  BattleRasterEffectTable23:
     MakeRasterEffectTable 10, ALIGN_SLICES
-  BattleRasterEffectTable24:
     MakeRasterEffectTable 10, SKEW_SLICES
-  BattleRasterEffectTable25:
-    MakeRasterEffectTable 12, ALIGN_SLICES
-  BattleRasterEffectTable26:
-    MakeRasterEffectTable 12, SKEW_SLICES
-
-
-  BattleRasterEffectTable27:
-    MakeRasterEffectTable 10, ALIGN_SLICES
-  BattleRasterEffectTable28:
-    MakeRasterEffectTable 10, SKEW_SLICES
-  BattleRasterEffectTable29:
-    MakeRasterEffectTable 12, ALIGN_SLICES
-  BattleRasterEffectTable30:
-    MakeRasterEffectTable 12, SKEW_SLICES
-  BattleRasterEffectTable31:
-    MakeRasterEffectTable 14, ALIGN_SLICES
-  BattleRasterEffectTable32:
-    MakeRasterEffectTable 14, SKEW_SLICES
-
-
-  BattleRasterEffectMetaTable:
-    ; A table of pointers to Battle Raster Effects which create the illusion
-    ; of a moving alien army.
-    .dw BattleRasterEffectTable1, BattleRasterEffectTable2,
-    .dw BattleRasterEffectTable3, BattleRasterEffectTable4,
-    .dw BattleRasterEffectTable5, BattleRasterEffectTable6,
-    .dw BattleRasterEffectTable7, BattleRasterEffectTable8,
-    .dw BattleRasterEffectTable9, BattleRasterEffectTable10,
-    .dw BattleRasterEffectTable11, BattleRasterEffectTable12,
-    .dw BattleRasterEffectTable13, BattleRasterEffectTable14,
-    .dw BattleRasterEffectTable15, BattleRasterEffectTable16,
-
-    .dw BattleRasterEffectTable17, BattleRasterEffectTable18,
-    .dw BattleRasterEffectTable19, BattleRasterEffectTable20,
-    .dw BattleRasterEffectTable21, BattleRasterEffectTable22,
-    .dw BattleRasterEffectTable23, BattleRasterEffectTable24,
-
-    ;.dw BattleRasterEffectTable25, BattleRasterEffectTable26,
-    ;.dw BattleRasterEffectTable27, BattleRasterEffectTable28,
-    ;.dw BattleRasterEffectTable29, BattleRasterEffectTable30,
-    ;.dw BattleRasterEffectTable31, BattleRasterEffectTable32,
-
-  BattleRasterEffectMetaTableEnd:
-
+    MakeRasterEffectTable 8, ALIGN_SLICES
+    MakeRasterEffectTable 8, SKEW_SLICES
+    MakeRasterEffectTable 6, ALIGN_SLICES
+    MakeRasterEffectTable 6, SKEW_SLICES
+    MakeRasterEffectTable 4, ALIGN_SLICES
+    MakeRasterEffectTable 4, SKEW_SLICES
+    MakeRasterEffectTable 2, ALIGN_SLICES
+    MakeRasterEffectTable 2, SKEW_SLICES
+    MakeRasterEffectTable 0, ALIGN_SLICES
+    MakeRasterEffectTable 0, SKEW_SLICES
+    MakeRasterEffectTable -2, ALIGN_SLICES
+    MakeRasterEffectTable -2, SKEW_SLICES
+    MakeRasterEffectTable -4, ALIGN_SLICES
+    MakeRasterEffectTable -4, SKEW_SLICES
+    MakeRasterEffectTable -6, ALIGN_SLICES
+    MakeRasterEffectTable -6, SKEW_SLICES
+    MakeRasterEffectTable -8, ALIGN_SLICES
+    MakeRasterEffectTable -8, SKEW_SLICES
+    MakeRasterEffectTable -10, ALIGN_SLICES
+    MakeRasterEffectTable -10, SKEW_SLICES
+    MakeRasterEffectTable -8, ALIGN_SLICES
+    MakeRasterEffectTable -8, SKEW_SLICES
+    MakeRasterEffectTable -6, ALIGN_SLICES
+    MakeRasterEffectTable -6, SKEW_SLICES
+    MakeRasterEffectTable -4, ALIGN_SLICES
+    MakeRasterEffectTable -4, SKEW_SLICES
+    MakeRasterEffectTable -2, ALIGN_SLICES
+    MakeRasterEffectTable -2, SKEW_SLICES
+  BattleRasterEffectTablesEnd:
 .ends
 
 ; -----------------------------------------------------------------------------
