47541a723a8dec26b70f27f1ada0bd13de96e505
hang-on <asje@me.com>
2016-07-12 16:17:45 +0200

    add base_buffer

--- Main.asm
+++ Main.asm
@@ -2,8 +2,11 @@
 .include "Invaderlib.inc"
 ;
 .equ VSCROLL_INIT_VALUE 223
+.equ VERTICAL_SCROLL_STEP 8
+.equ VERTICAL_SCROLL_LIMIT 183
 .equ TIMER_INIT_VALUE 120
 .equ RASTER_INIT_VALUE 7
+.equ BASE_SIZE 5*3              ; 5 chars wide, 3 chars high.
 ;
 .bank 0 slot 0
 .org $0038
@@ -28,6 +31,7 @@
 .ramsection "Main variables" slot 3
   vertical_scroll_value db
   vertical_scroll_timer db
+  base_buffer dsb BASE_SIZE*2   ; X 2 because we are storing name table words.
 .ends
 .bank 0 slot 0
 ; -----------------------------------------------------------------------------
@@ -42,6 +46,8 @@
     ;
     LOAD_IMAGE MockupAssets,MockupAssetsEnd
     ;
+
+    ; Turn on screen, etc.
     ld hl,register_data
     call load_vdp_registers
     ; Skip an interrupt to make sure that we start main at vblank.
@@ -58,22 +64,24 @@
     ;
     ; Non-vblank stuff below this line...
     ;
-    ld a,(vertical_scroll_timer)      ; Get vertical_scroll_timer.
-    or a                              ; Is it zero?
+    ld a,(vertical_scroll_timer)
+    or a
     jp nz,decrement_timer
-      ld a,TIMER_INIT_VALUE           ; Yes - load init value.
-      ld (vertical_scroll_timer),a    ;
+      ; Time is up - handle scrolling.
+      ld a,TIMER_INIT_VALUE
+      ld (vertical_scroll_timer),a
       ld a,(vertical_scroll_value)
-      sub 8
-      cp 183
+      sub VERTICAL_SCROLL_STEP
+      cp VERTICAL_SCROLL_LIMIT
       jp nz,+
         ld a,VSCROLL_INIT_VALUE
       +:
       ld (vertical_scroll_value),a
       jp vertical_scroll_end                           ;
-    decrement_timer:                  ; No - decrement timer.
-      dec a                           ;
-      ld (vertical_scroll_timer),a    ;
+    decrement_timer:
+      ; Not time for scrolling yet - just decrement the timer.
+      dec a
+      ld (vertical_scroll_timer),a
     vertical_scroll_end:
     ;
   jp main
